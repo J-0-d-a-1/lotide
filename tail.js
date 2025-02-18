7afa773ead8a658e0daa7be00021513dee33b86a
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-17 18:57:05 -0800

    tail.js fixed with slice

--- tail.js
+++ tail.js
@@ -7,12 +7,13 @@ const assertEqual = function (actual, expected) {
 };
 
 const tail = function (arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i > 0) {
-      newArr.push(arr[i]);
-    }
-  }
+  // let newArr = [];
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (i > 0) {
+  //     newArr.push(arr[i]);
+  //   }
+  // }
+  let newArr = arr.slice(1);
   return newArr;
 };
 
@@ -22,3 +23,8 @@ assertEqual(result.length, 2);
 assertEqual(result[0], words[1]);
 assertEqual(result[1], words[2]);
 assertEqual(words.length, 3);
+
+const word = ["Bootcamp"];
+const result2 = tail(word);
+assertEqual(result2.length, 0);
+assertEqual(word.length, 1);
