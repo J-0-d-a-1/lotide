4fc3c147f9dfd3188e726d2dd100cf124e2d7248
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-17 17:17:11 -0800

    assertEqual.js added

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function (actural, expected) {
+  if (actural === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actural + " === " + expected);
+  } else {
+    console.log("🔴🔴🔴 Assertion Failed: " + actural + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Learn", "Learn");
+assertEqual("Lern", "Learn");
+assertEqual(2, 2);
+assertEqual(2, 2.2);
