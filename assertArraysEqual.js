988da6a369aac6ad78d1250cd599ef02a521f813
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-18 20:01:12 -0800

    assertArraysEqual.js updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,42 +1,41 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+const eqArrays = function (arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
   }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // step1: input 2 arrays, output assert message
 
 // step2:
 // if it not 2 arrays -> false
-// if arr1.length !== arr2.length -> `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`
+// if !eqArrays(arr1, arr2) -> `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`
 
 // step3: between input and output
 //  assertArraysEqual function
-//    iterate over the 2 arrays
-//      arr1[i] !== arr2[i] -> `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`
-//      return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`
+//     !eqArrays(arr1, arr2) -> `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`
+//     return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`
 
 const assertArraysEqual = function (arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    return false;
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   }
-
-  if (arr1.length !== arr2.length) {
-    return `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`;
-    }
-  }
-
-  return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [1, 2, "3"]));
-console.log(assertArraysEqual(1, [1]));
-console.log(assertArraysEqual(["light", "house"], ["light", "house"]));
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+assertArraysEqual(1, [1]);
+assertArraysEqual(["light", "house"], ["light", "house"]);
