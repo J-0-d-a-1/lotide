9239955fc31e9ada12b1d461cb269cdd3be8bea7
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-18 19:18:12 -0800

    eqArrays.js added

---
+++ eqArrays.js
@@ -0,0 +1,42 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// step1: input arrays, output boolean
+
+// step2: figure out behavior depending on data
+//  !Array.isArray(arr1) || !Array.isArray(arr2) -> Give me 2 arrays
+//  arr1.length !== arr2.length -> return false
+
+// step3: between input and output
+//  Itrete over the two arrays
+//    If arr1[i] doesn't equal arr2[i] return false
+//    return true;
+//
+
+const eqArrays = function (arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return "Give me 2 arrays 😡";
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
