f995ecb5ff81748ba56695172e46125dd59d96df
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-19 19:00:49 -0800

    findKeyByValue.js added

---
+++ findKeyByValue.js
@@ -0,0 +1,45 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// input object and value, output key(a string)
+
+// if object doesnt have key of the given value: return undifned;
+
+// define resultKey;
+// for of loop (Object.key())
+// for (const key of Object.key(object))
+//  if object[key] === value
+//    resultKey = key
+//  else reslutKey = undifined
+// return resultKey;
+
+const findKeyByValue = function (object, value) {
+  let resultKey = undefined;
+
+  for (const key of Object.keys(object)) {
+    if (object[key] === value) {
+      resultKey = key;
+    }
+  }
+
+  return resultKey;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Expans"));
+// console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
