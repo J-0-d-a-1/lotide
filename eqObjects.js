926bbba1a32ed43e3ffb1e5d7cc6ef1b4295d7b1
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-19 20:25:03 -0800

    eqObjects.js updated with feedback

--- eqObjects.js
+++ eqObjects.js
@@ -32,15 +32,15 @@ const eqArrays = function (arr1, arr2) {
 const eqObjects = function (object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } else {
-    for (const key of Object.keys(object1)) {
-      if (Array.isArray(object1[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]) {
+  }
+
+  for (const key of Object.keys(object1)) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+    } else if (object1[key] !== object2[key]) {
+      return false;
     }
   }
 
