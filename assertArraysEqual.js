354029a18c07ca8b95b60bbcff249fde2eca2e71
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-24 15:20:08 -0800

    test code moved to assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,11 +19,4 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4, 4]);
-assertArraysEqual([1, 2, 3, 4, 4], [1, 2, 3, 4, 4]);
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
-assertArraysEqual(1, [1]);
-assertArraysEqual(["light", "house"], ["light", "house"]);
-
 module.exports = assertArraysEqual;
