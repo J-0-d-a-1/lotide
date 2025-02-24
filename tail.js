eb442ec2767c2fbc61676b486c467813f57fca09
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-24 14:04:26 -0800

    tail.js exported and test code moved to tailTest.js

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function (arr) {
   // let newArr = [];
   // for (let i = 0; i < arr.length; i++) {
@@ -17,14 +9,4 @@ const tail = function (arr) {
   return newArr;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const result = tail(words);
-assertEqual(result.length, 2);
-assertEqual(result[0], words[1]);
-assertEqual(result[1], words[2]);
-assertEqual(words.length, 3);
-
-const word = ["Bootcamp"];
-const result2 = tail(word);
-assertEqual(result2.length, 0);
-assertEqual(word.length, 1);
+module.exports = tail;
