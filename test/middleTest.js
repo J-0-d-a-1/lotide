9c66d8a440cef8b4a4acc1a3c42bfe276c46ba6c
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-24 16:46:46 -0800

    test code updated with mocha and chah

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,19 @@
+const assert = require("chai").assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+
+describe("@middle", () => {
+  it("return [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("return [] for an empty array", () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
