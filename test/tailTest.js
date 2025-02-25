bfaf8a686f55562af0855f17269506861a83d890
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-24 16:39:21 -0800

    tailTest.js test code updated with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,5 @@
-// const assertEqual = require("../assertEqual");
 const assert = require("chai").assert;
 const tail = require("../tail");
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// const result = tail(words);
-// assertEqual(result.length, 2);
-// assertEqual(result[0], words[1]);
-// assertEqual(result[1], words[2]);
-// assertEqual(words.length, 3);
-
-// const word = ["Bootcamp"];
-// const result2 = tail(word);
-// assertEqual(result2.length, 0);
-// assertEqual(word.length, 1);
-
-// const emptyArr = [];
-// const result3 = tail(emptyArr);
-// assertEqual(result3.length, 0);
 
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
